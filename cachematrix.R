507db6f1983a8719a64869d3602cdfce9c4a1399
agniesiia <agniesiia@users.noreply.github.com>
2015-08-23 20:17:03 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(xArg = matrix()) { # assign name 'makeCacheMatrix' to function expecting one argument, where argument xArgs is a matrix.
+ #set a list of arguments that will be used
+  inverseArg <- NULL
+  set<-function(bArg){ # uses function 'set' with one argument bArg, where matrix xArg is assiged to bArg
+  xArg<<-bArg
+}
 
-## Write a short comment describing this function
+# get a function with the supplied argument
+get<-function() xArg
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#passing solve as inversArg
+setInverse<-function(solve) inverseArg <<- solve
+#getinverse matrix taking inversArg as solve
+getInverse<-function() inverseArg
+#creating a list of set/get of the reverse matrix
+list(set=set, get=get,
+   setInverse=setInverse,
+   getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(xArg=matrix(), ...) {
+#passing the xArg with the getmatrix function to the inverseArg
+    inverseArg<-xArg$getInverse()
+    #catch statement for the null argument of inverseArg 
+    #if the inverse matrix has been calculated it will print it
+    if(!is.null(inverseArg)){
+      message("getting cached data")
+      return(inverseArg)
+    }
+#inversing the matrix and printing it
+  inverseArg <- solve(xArg$get())
+  xArg$setInverse(inverseArg)
+  inverseArg
 }
